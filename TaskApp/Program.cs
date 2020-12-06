0daf25e598532acfad2d79ab03c8f14740b32ef4
MariaTrunk <m.trunkova@gmail.com>
2020-12-06 14:18:09 +0300

    TaskLesson29112020

---
+++ TaskApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TaskApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            Task<int> summ = new Task<int>(() =>
+            {
+                
+                for (int i = 1; i < 101; i++)
+                {
+                    sum += i;
+                }
+                return sum;
+                
+            });
+            summ.Start();
+            Console.WriteLine(summ.Result);
+            Console.ReadLine();
+        }
+    }
+}
